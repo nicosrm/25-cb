cedbf232d27570223dcedbcbdbf9e24bd968d6b1
nicosrm <86198952+nicosrm@users.noreply.github.com>
2025-10-13 15:02:26 +0200

    feat: abstract syntax for local names

--- compiler/I.hs
+++ compiler/I.hs
@@ -3,6 +3,20 @@ data Exp
     = Literal Integer
     | Plus Exp Exp
     | Times Exp Exp
+    | Let Name Exp Exp -- Name für Variable, Zuweisung, Ausdruck
+    | Ref Name
+    deriving Show
+
+type Name = String
+
+ex1 :: Exp
+ex1 = Times (Plus (Literal 1) (Literal 2)) (Literal 3)
+
+ex2 :: Exp
+ex2 = Let
+    "x"
+    (Plus (Literal 3)  (Literal 4))
+    (Times (Ref "x") (Ref "x"))
 
 -- Interpreter: AST -> Wert
 value :: Exp -> Integer
