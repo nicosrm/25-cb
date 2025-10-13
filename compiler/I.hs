630a0b7430300974306f4043e344b5df825870fe
nicosrm <86198952+nicosrm@users.noreply.github.com>
2025-10-13 15:15:48 +0200

    feat: add env and extend

--- compiler/I.hs
+++ compiler/I.hs
@@ -18,9 +18,22 @@ ex2 = Let
     (Plus (Literal 3)  (Literal 4))
     (Times (Ref "x") (Ref "x"))
 
+ex3 :: Exp
+ex3 = Let "x" (Literal 2)
+    $ Let "y" (Literal 3)
+    $ Ref "x"
+
+type Env = (Name -> Integer)
+
+extend :: Name -> Integer -> Env -> Env
+extend n v env = \ m -> if m == n then v else env m
+
 -- Interpreter: AST -> Wert
-value :: Exp -> Integer
-value x = case x of
+value :: Env -> Exp -> Integer
+value env x = case x of
     Literal i -> i
-    Plus l r -> value l + value r
-    Times l r -> value l * value r
+    Plus l r -> value env l + value env r
+    Times l r -> value env l * value env r
+    Ref n -> env n
+    Let n x b -> 
+        value (extend n (value env x) env) b
