885401c65ad77530f83dc99babc69b990e26decb
nicosrm <86198952+nicosrm@users.noreply.github.com>
2025-10-17 11:02:04 +0200

    refactor: env as map

--- compiler/I.hs
+++ compiler/I.hs
@@ -1,3 +1,6 @@
+import Prelude hiding (lookup)
+import qualified Data.Map as M
+
 -- Menge an AST
 data Exp
     = Literal Integer
@@ -39,13 +42,35 @@ ex6 = Let "x" (Eq (Literal 1) (Literal 42))
 ex7 :: Exp
 ex7 = Ref "y"
 
--- value (extend "y" 42 undefined) $ Ref "y" -- 42
--- value (extend "y" 42 undefined) $ Ref "x" -- undefined
+-- value (extend "y" 42 env0) $ Ref "y" -- 42
+-- value (extend "y" 42 env0) $ Ref "x" -- undefined
 
-type Env = (Name -> Integer)
+ex8 :: Exp
+ex8 = Let "x" (Ref "x") (Ref "x")
+-- x erst im dritten Argument sichtbar
+
+ex9 :: Exp
+ex9 = Let "x" (Ref "y")
+    $ Let "y" (Literal 0) (Ref "x")
+
+ex10 :: Exp
+ex10 = Let "x" (Literal 0)
+    $ Let "y" (Ref "y") (Ref "y")
+
+-- type Env = (Name -> Integer)
+type Env = M.Map Name Integer
+
+-- leere Umgebung
+env0 = M.empty
 
 extend :: Name -> Integer -> Env -> Env
-extend n v env = \ m -> if m == n then v else env m
+-- extend n v env = \ m -> if m == n then v else env m
+extend = M.insert
+
+lookup :: Env -> Name -> Integer
+lookup env n = case M.lookup n env of
+    Nothing -> error $ "lookup (" <> show env <> "): undefined variable " <> n
+    Just i -> i
 
 -- Interpreter: AST -> Wert
 value :: Env -> Exp -> Integer
@@ -56,7 +81,7 @@ value env x = case x of
     Eq l r -> if value env l == value env r
         then 1
         else 0
-    Ref n -> env n
+    Ref n -> lookup env n
     Let n x b -> 
         value (extend n (value env x) env) b
     If c t f -> if (value env c) /= 0 
